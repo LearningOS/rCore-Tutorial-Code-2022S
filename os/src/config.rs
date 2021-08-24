14204dc6542828a2b99cd52fddf1caf8dce79615
zhanghx0905 <731931282@qq.com>
2021-08-24 11:47:24 +0800

    fix user stack size 8092 -> 4096

--- os/src/config.rs
+++ os/src/config.rs
@@ -1,4 +1,4 @@
-pub const USER_STACK_SIZE: usize = 4096 * 2;
+pub const USER_STACK_SIZE: usize = 4096;
 pub const KERNEL_STACK_SIZE: usize = 4096 * 2;
 pub const MAX_APP_NUM: usize = 8;
 pub const APP_BASE_ADDRESS: usize = 0x80400000;
