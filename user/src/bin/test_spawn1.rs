9e65ed5c6709daf451fd1db8e9153c35143c4d49
zhanghx0905 <731931282@qq.com>
2021-08-25 11:32:42 +0800

    update testcases

--- user/src/bin/test_spawn1.rs
+++ user/src/bin/test_spawn1.rs
@@ -15,7 +15,7 @@ use user_lib::{spawn, wait, waitpid};
 
 #[no_mangle]
 pub fn main() -> i32 {
-    let cpid = spawn("ch5_exit0\0");
+    let cpid = spawn("test_exit0\0");
     assert!(cpid >= 0, "child pid invalid");
     println!("new child {}", cpid);
     let mut exit_code: i32 = 0;
@@ -23,7 +23,7 @@ pub fn main() -> i32 {
     assert_eq!(exit_pid, cpid, "error exit pid");
     assert_eq!(exit_code, 66778, "error exit code");
     println!("Test wait OK!");
-    let (cpid0, cpid1) = (spawn("ch5_exit0\0"), spawn("ch5_exit1\0"));
+    let (cpid0, cpid1) = (spawn("test_exit0\0"), spawn("test_exit1\0"));
     let exit_pid = waitpid(cpid1 as usize, &mut exit_code);
     assert_eq!(exit_pid, cpid1, "error exit pid");
     assert_eq!(exit_code, -233, "error exit code");
