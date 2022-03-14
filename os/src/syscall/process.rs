330890b28f567c987eb50d35a5c4fa51dc96275a
田凯夫 <tkf19@mails.tsinghua.edu.cn>
2022-03-14 16:37:10 +0800

    Update docs

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -15,17 +15,20 @@ pub struct TaskInfo {
     time: usize,
 }
 
+/// task exits and submit an exit code
 pub fn sys_exit(exit_code: i32) -> ! {
     info!("[kernel] Application exited with code {}", exit_code);
     exit_current_and_run_next();
     panic!("Unreachable in sys_exit!");
 }
 
+/// current task gives up resources for other tasks
 pub fn sys_yield() -> isize {
     suspend_current_and_run_next();
     0
 }
 
+/// get time with second and microsecond
 pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
     let us = get_time_us();
     unsafe {
@@ -37,7 +40,7 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
     0
 }
 
-// YOUR JOB: Finish sys_task_info to pass testcases
+/// YOUR JOB: Finish sys_task_info to pass testcases
 pub fn sys_task_info(ti: *mut TaskInfo) -> isize {
     -1
 }
--- os/src/trap/mod.rs
+++ os/src/trap/mod.rs
@@ -11,6 +11,7 @@ use riscv::register::{
 
 core::arch::global_asm!(include_str!("trap.S"));
 
+/// initialize CSR `stvec` as the entry of `__alltraps`
 pub fn init() {
     extern "C" {
         fn __alltraps();
@@ -20,6 +21,7 @@ pub fn init() {
     }
 }
 
+/// timer interrupt enabled
 pub fn enable_timer_interrupt() {
     unsafe {
         sie::set_stimer();
@@ -27,9 +29,10 @@ pub fn enable_timer_interrupt() {
 }
 
 #[no_mangle]
+/// handle an interrupt, exception, or system call from user space
 pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
-    let scause = scause::read();
-    let stval = stval::read();
+    let scause = scause::read(); // get trap cause
+    let stval = stval::read(); // get extra value
     match scause.cause() {
         Trap::Exception(Exception::UserEnvCall) => {
             cx.sepc += 4;
