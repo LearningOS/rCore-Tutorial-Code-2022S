150691ae2fa08b37b78ca532d5e582279f09d475
Clement5140 <Clement5140@users.noreply.github.com>
2022-03-25 11:28:18 +0000

    deploy: d6f1ef3e30db57cb386333df6aded316942c138c

--- ch4/os/logging/sidebar-items.js
+++ ch4/os/logging/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["init",""]],"struct":[["SimpleLogger",""]]});
+initSidebarItems({"fn":[["init","initiate logger"]],"struct":[["SimpleLogger","a simple logger"]]});
--- ch4/os/mm/sidebar-items.js
+++ ch4/os/mm/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["init",""]],"mod":[["address",""],["frame_allocator",""],["heap_allocator",""],["memory_set",""],["page_table",""]]});
+initSidebarItems({"fn":[["init","initiate heap allocator, frame allocator and kernel space"]],"mod":[["address",""],["frame_allocator","//! Implementation of [`FrameAllocator`] which  controls all the frames in the operating system."],["heap_allocator","The global allocator"],["memory_set",""],["page_table",""]]});
