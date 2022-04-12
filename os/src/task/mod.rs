8ca43b8455bb468997d91e3d44573c046d1b4ef1
msyzzz <1258647557@qq.com>
2022-04-12 15:23:31 +0800

    modify initproc_name

--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -90,7 +90,7 @@ lazy_static! {
     /// the name "initproc" may be changed to any other app name like "usertests",
     /// but we have user_shell, so we don't need to change it.
     pub static ref INITPROC: Arc<TaskControlBlock> = Arc::new(TaskControlBlock::new(
-        get_app_data_by_name("ch5_initproc").unwrap()
+        get_app_data_by_name("ch5b_initproc").unwrap()
     ));
 }
 
